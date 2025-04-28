df112851eb41a2ec78664e79d2b180356e7e3e0a
Alexandr Ukraintsev <sadman1337322@gmail.com>
2025-04-29 00:12:04 +0300

    задание на классы

--- src/Classes.ts
+++ src/Classes.ts
@@ -6,9 +6,9 @@
 - Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
  */
 export class Counter {
-    private counter: number;
+    private counter: number = 0;
 
-    constructor(start: number = 0) {
+    constructor(start: number) {
         this.counter = start;
     }
 
